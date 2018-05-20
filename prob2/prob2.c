416ed303c6e9c30ccab620aa80fd70c7ad327a37
RedChiken <computer6561@gmail.com>
2018-05-20 15:22:46 +0900

    done

--- prob2/prob2.c
+++ prob2/prob2.c
@@ -12,29 +12,36 @@ pthread_t p_thread[MAX_CAR];
 pthread_mutex_t mutex_lock;
 pthread_cond_t cond_wait;
 
+void enter(int id){
+    while(places < 0){
+        pthread_mutex_lock(&mutex_lock);
+        pthread_cond_wait(&cond_wait, &mutex_lock);
+        pthread_mutex_unlock(&mutex_lock);
+    }
+    pthread_mutex_lock(&mutex_lock);
+    places--;
+    pthread_mutex_unlock(&mutex_lock);
+    printf("Car %d: entered\n", id);
+}
+
+void leave(int id){
+    pthread_mutex_lock(&mutex_lock);
+    places++;
+    pthread_cond_broadcast(&cond_wait);
+    pthread_mutex_unlock(&mutex_lock);
+    printf("Car %d: left\n", id);
+}
+
 void *car(void *threadNum){
-    printf("position %d\n", 4);
     int *temp = threadNum;
     int id = (*temp) + 1;
     while(1){
         sleep(1);
-        
-        while(places == 0){
-            pthread_cond_wait(&cond_wait, &mutex_lock);
-        }
-        pthread_mutex_lock(&mutex_lock);
-        places--;
-        pthread_mutex_unlock(&mutex_lock);
-        printf("Car %d: entered\n", id);
-        
+        enter(id);
         sleep(2);
-        
-        pthread_mutex_lock(&mutex_lock);
-        places++;
-        pthread_mutex_unlock(&mutex_lock);
-        pthread_cond_broadcast(&cond_wait);
-        printf("Car %d: left\n", id);
+        leave(id);
     }
+    pthread_exit(0);
     return 0;
 }
 
@@ -43,17 +50,16 @@ int main(int argc, char *argv[]){
     int status = 0;
     pthread_mutex_init(&mutex_lock, 0);
     pthread_cond_init(&cond_wait, 0);
-    printf("position %d\n", 1);
     for(iter = 0; iter < MAX_CAR; iter++){
         if(pthread_create(&p_thread[iter], 0, car, (void *)&iter) < 0){
             perror("thread create error: ");
             exit(0);
         }
     }
-    printf("position %d\n", 2);
     for(iter = 0; iter < MAX_CAR; iter++){
         pthread_join(p_thread[iter], (void **)&status);
     }
-    printf("position %d\n", 3);
+    pthread_mutex_destroy(&mutex_lock);
+    pthread_cond_destroy(&cond_wait);
     return 0;
 }
